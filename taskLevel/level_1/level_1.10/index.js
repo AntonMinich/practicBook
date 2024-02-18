068248e53e43900a5898d07520cdcf03fe10f11a
Anton Minich <ant.minich@gmail.com>
2024-02-18 17:39:49 +0300

    completed task level 1.10

--- taskLevel/level_1/level_1.10/index.js
+++ taskLevel/level_1/level_1.10/index.js
@@ -11,3 +11,56 @@ for (let i = 1; i <= 100; i++) {
 }
 console.log(arr2);
 
+// 
+
+
+// Дано некоторое число: 12345 Выведите в консоль все его символы с конца.
+let number = 12345;
+let arrNumber = [...String(number)];
+for (let i = arrNumber.length; i >= 0; i--) {
+    console.log(arrNumber[i]);
+}
+
+// 
+number = 12345;
+arrNumber = [...String(number)];
+arrNumber.forEach((elem, index, arr) => console.log(arr[arr.length-1 - index]));
+console.log(arrNumber);
+
+
+// Дан некоторый массив, например, вот такой: [1, 2, 3, 4, 5, 6] По очереди выведите в консоль подмассивы из двух элементов нашего массива:[1, 2][3, 4][5, 6]
+
+
+let array = [1, 2, 3, 4, 5, 6];
+let newArray = [];
+
+for (let i = 0; i < array.length; i += 2) {
+    newArray.push([array[i], array[i + 1]]);
+}
+
+console.log(newArray);
+
+
+// Даны два массива: let arr1 = [1, 2, 3]; let arr2 = [4, 5, 6]; Слейте эти массивы в новый массив: [1, 2, 3, 4, 5, 6]
+
+let arr3 = [1, 2, 3];
+let arr4 = [4, 5, 6];
+let res = arr3.concat(arr4);
+console.log(res);
+
+
+// 
+
+arr3 = [1, 2, 3];
+arr4 = [4, 5, 6];
+res = [...arr3,...arr4];
+console.log(res);
+
+// 
+
+arr3 = [1, 2, 3];
+arr4 = [4, 5, 6];
+res = [];
+arr3.forEach(elem => res.push(elem));
+arr4.forEach(elem => res.push(elem));
+console.log(res);
